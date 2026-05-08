a658b421edcec79c00d1122420cb3b53f8b60a80
MatiasSagredo <ma.sagredom@duocuc.cl>
2026-05-08 18:42:01 -0400

    feat: create RolRepository

---
+++ src/main/java/com/antiinfernum/auth/repository/RolRepository.java
@@ -0,0 +1,8 @@
+package com.antiinfernum.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.antiinfernum.auth.model.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, String> {
+}
