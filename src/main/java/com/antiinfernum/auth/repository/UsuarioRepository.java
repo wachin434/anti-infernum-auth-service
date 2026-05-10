740b28d2033cab913fd9a386905661c26703187b
MatiasSagredo <ma.sagredom@duocuc.cl>
2026-05-10 18:46:28 -0400

    feat: create UsuarioService and add Spring Security

--- src/main/java/com/antiinfernum/auth/repository/UsuarioRepository.java
+++ src/main/java/com/antiinfernum/auth/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.antiinfernum.auth.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.antiinfernum.auth.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
+    Optional<Usuario> findByEmail(String email);
 }
